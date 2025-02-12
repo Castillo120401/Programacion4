bb3b6c3a1fbca2ca845540838f246762a100f043
Castillo120401 <brayanomar.castllofuentes@unidemat.edu.mx>
2025-02-12 12:13:25 -0600

    Add files via upload

---
+++ PRACTICA3/PRACTICA3/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace PRACTICA3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblR1 = new System.Windows.Forms.Label();
+            this.lblR2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.txt1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(32, 101);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 145);
+            this.panel1.TabIndex = 0;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(24, 83);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(221, 26);
+            this.txt1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INGRESA EL RADIO DEL CIRCULO";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(348, 70);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(130, 81);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "CALCULAR AREA";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(348, 184);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(130, 81);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "CALCULAR PERIMETRO";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(522, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RESULTADO";
+            // 
+            // lblR1
+            // 
+            this.lblR1.AutoSize = true;
+            this.lblR1.Font = new System.Drawing.Font("Myanmar Text", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblR1.Location = new System.Drawing.Point(546, 79);
+            this.lblR1.Name = "lblR1";
+            this.lblR1.Size = new System.Drawing.Size(0, 63);
+            this.lblR1.TabIndex = 4;
+            // 
+            // lblR2
+            // 
+            this.lblR2.AutoSize = true;
+            this.lblR2.Font = new System.Drawing.Font("Myanmar Text", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblR2.Location = new System.Drawing.Point(546, 202);
+            this.lblR2.Name = "lblR2";
+            this.lblR2.Size = new System.Drawing.Size(0, 63);
+            this.lblR2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblR2);
+            this.Controls.Add(this.lblR1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "AREA Y PERIMETRO - CIRCULO";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblR1;
+        private System.Windows.Forms.Label lblR2;
+    }
+}
+
---
+++ PRACTICA3/PRACTICA3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRACTICA3
+{
+    public partial class Form1 : Form
+    {
+     
+ 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+         double Radio = Convert.ToDouble(txt1.Text);
+         double varea = Math.PI * Math.Pow(Radio, 2);
+            lblR1.Text = varea.ToString("F2");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            double radio = Convert.ToDouble(txt1.Text);
+            double P = 2 * Math.PI * radio;
+            lblR2.Text = P.ToString("f2");
+
+        }
+    }
+}
