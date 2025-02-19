997be2b8f9ada55d0cf2370bc8399bf204f37ba3
Castillo120401 <brayanomar.castllofuentes@unidemat.edu.mx>
2025-02-19 11:33:14 -0600

    Add files via upload

---
+++ PRACTICA5/PRACTICA5/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace PRACTICA5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.cbboxP = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.dgvN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbboxP
+            // 
+            this.cbboxP.FormattingEnabled = true;
+            this.cbboxP.Location = new System.Drawing.Point(480, 84);
+            this.cbboxP.Name = "cbboxP";
+            this.cbboxP.Size = new System.Drawing.Size(121, 28);
+            this.cbboxP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(409, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PAIS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "APELLIDO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NOMBRE";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(172, 36);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(153, 26);
+            this.txtN.TabIndex = 4;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(172, 88);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(153, 26);
+            this.txtA.TabIndex = 5;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(172, 130);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(105, 33);
+            this.btnagregar.TabIndex = 6;
+            this.btnagregar.Text = "AGREGAR";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(369, 130);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(105, 33);
+            this.btnBorrar.TabIndex = 7;
+            this.btnBorrar.Text = "BORRAR";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.BackgroundColor = System.Drawing.Color.White;
+            this.dgvDatos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDatos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvN,
+            this.dgvA,
+            this.dgvP});
+            this.dgvDatos.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvDatos.Location = new System.Drawing.Point(85, 169);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowHeadersWidth = 62;
+            this.dgvDatos.RowTemplate.Height = 28;
+            this.dgvDatos.Size = new System.Drawing.Size(526, 278);
+            this.dgvDatos.TabIndex = 8;
+            this.dgvDatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellClick);
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellContentClick);
+            // 
+            // dgvN
+            // 
+            this.dgvN.Frozen = true;
+            this.dgvN.HeaderText = "NOMBRE";
+            this.dgvN.MinimumWidth = 8;
+            this.dgvN.Name = "dgvN";
+            this.dgvN.Width = 150;
+            // 
+            // dgvA
+            // 
+            this.dgvA.Frozen = true;
+            this.dgvA.HeaderText = "APELLIDO";
+            this.dgvA.MinimumWidth = 8;
+            this.dgvA.Name = "dgvA";
+            this.dgvA.Width = 150;
+            // 
+            // dgvP
+            // 
+            this.dgvP.Frozen = true;
+            this.dgvP.HeaderText = "PAIS";
+            this.dgvP.MinimumWidth = 8;
+            this.dgvP.Name = "dgvP";
+            this.dgvP.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(668, 506);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbboxP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbboxP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvP;
+    }
+}
+
---
+++ PRACTICA5/PRACTICA5/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRACTICA5
+{
+    public partial class Form1 : Form
+    {
+        // No guarda el indice selecionado en la tabla de dgvDatos
+        int vindice=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //linea de codigo para agregar en el combobox
+            cbboxP.Items.Add("Mexico");
+            cbboxP.Items.Add("Alemania"); 
+            cbboxP.Items.Add("Belgica");
+            cbboxP.Items.Add("España");
+            cbboxP.Items.Add("China");
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            //se agrega una variable en tipo texto para hacer cadena de texto
+            string vseleccion = Convert.ToString(cbboxP.Text);
+            //linea de codigo que almacena en la variable indice
+            int vindice = dgvDatos.Rows.Add();
+            //lineas de codigo para que almacene su valor de cada dato en su celda correspondiente
+            dgvDatos.Rows[vindice].Cells[0].Value = txtN.Text;
+            dgvDatos.Rows[vindice].Cells[1].Value= txtA.Text;
+            dgvDatos.Rows[vindice].Cells[2].Value = vseleccion;
+            //borrar automaticamente
+            txtN.Text = "";
+            txtA.Text = "";
+            cbboxP.Text = "";
+        }
+
+        private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            //condicional con la que evualuamos que si 'vindice' es diferente o su valor no es -1 
+            //entonces procoedemos a eliminar el item selecionado de lo contrario moastrara un msj de error
+            if (vindice != -1)
+            {
+                //es para no borrar el encabezado
+                dgvDatos.Rows.RemoveAt(vindice);
+            }
+             else
+            {
+                //muestra mensaje de error
+                MessageBox.Show("Seleccion de item incorrecto");
+            }
+            
+        }
+
+        private void dgvDatos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            vindice = e.RowIndex;
+        }
+    }
+}
