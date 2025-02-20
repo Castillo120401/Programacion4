7c794a03d14d1ce4109a5b21b1d58eba4a738ef4
Castillo120401 <brayanomar.castllofuentes@unidemat.edu.mx>
2025-02-20 10:26:18 -0600

    Add files via upload

---
+++ Practica 5.1/Practica 5.1/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Practica_5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtP3 = new System.Windows.Forms.TextBox();
+            this.txtP2 = new System.Windows.Forms.TextBox();
+            this.txtP1 = new System.Windows.Forms.TextBox();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblP = new System.Windows.Forms.Label();
+            this.lblE = new System.Windows.Forms.Label();
+            this.lblN = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.N = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(333, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PROMEDIOS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(55, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 27);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "PARCIAL 1 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 27);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PARCIAL 3 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(55, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 27);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "PARCIAL 2 :";
+            // 
+            // txtP3
+            // 
+            this.txtP3.Location = new System.Drawing.Point(222, 216);
+            this.txtP3.Name = "txtP3";
+            this.txtP3.Size = new System.Drawing.Size(100, 26);
+            this.txtP3.TabIndex = 9;
+            // 
+            // txtP2
+            // 
+            this.txtP2.Location = new System.Drawing.Point(222, 180);
+            this.txtP2.Name = "txtP2";
+            this.txtP2.Size = new System.Drawing.Size(100, 26);
+            this.txtP2.TabIndex = 10;
+            // 
+            // txtP1
+            // 
+            this.txtP1.Location = new System.Drawing.Point(222, 142);
+            this.txtP1.Name = "txtP1";
+            this.txtP1.Size = new System.Drawing.Size(100, 26);
+            this.txtP1.TabIndex = 11;
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(194, 260);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(111, 54);
+            this.btnC.TabIndex = 12;
+            this.btnC.Text = "CALCULAR\r\nPROMEDIO";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(320, 258);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(111, 54);
+            this.btnB.TabIndex = 14;
+            this.btnB.Text = "BORRAR";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(454, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 27);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "PROMEDIO";
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.BackColor = System.Drawing.Color.Transparent;
+            this.lblP.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP.Location = new System.Drawing.Point(521, 142);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(0, 27);
+            this.lblP.TabIndex = 16;
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.BackColor = System.Drawing.Color.Transparent;
+            this.lblE.Font = new System.Drawing.Font("Cooper Black", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblE.Location = new System.Drawing.Point(80, 346);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(0, 55);
+            this.lblE.TabIndex = 17;
+            // 
+            // lblN
+            // 
+            this.lblN.AutoSize = true;
+            this.lblN.BackColor = System.Drawing.Color.Transparent;
+            this.lblN.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblN.Location = new System.Drawing.Point(55, 97);
+            this.lblN.Name = "lblN";
+            this.lblN.Size = new System.Drawing.Size(138, 27);
+            this.lblN.TabIndex = 18;
+            this.lblN.Text = "NOMBRE :";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(222, 100);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 26);
+            this.txtN.TabIndex = 19;
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.N,
+            this.P1,
+            this.P2,
+            this.P3,
+            this.P});
+            this.dgvDatos.Location = new System.Drawing.Point(36, 346);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowHeadersWidth = 62;
+            this.dgvDatos.RowTemplate.Height = 28;
+            this.dgvDatos.Size = new System.Drawing.Size(664, 258);
+            this.dgvDatos.TabIndex = 20;
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellContentClick);
+            // 
+            // N
+            // 
+            this.N.HeaderText = "NOMBRE";
+            this.N.MinimumWidth = 8;
+            this.N.Name = "N";
+            this.N.Width = 150;
+            // 
+            // P1
+            // 
+            this.P1.HeaderText = "PARCIAL 1";
+            this.P1.MinimumWidth = 8;
+            this.P1.Name = "P1";
+            this.P1.Width = 150;
+            // 
+            // P2
+            // 
+            this.P2.HeaderText = "PARCIAL 2";
+            this.P2.MinimumWidth = 8;
+            this.P2.Name = "P2";
+            this.P2.Width = 150;
+            // 
+            // P3
+            // 
+            this.P3.HeaderText = "PARCIAL 3";
+            this.P3.MinimumWidth = 8;
+            this.P3.Name = "P3";
+            this.P3.Width = 150;
+            // 
+            // P
+            // 
+            this.P.HeaderText = "PROMEDIO";
+            this.P.MinimumWidth = 8;
+            this.P.Name = "P";
+            this.P.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 636);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.lblN);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.lblP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.txtP1);
+            this.Controls.Add(this.txtP2);
+            this.Controls.Add(this.txtP3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtP3;
+        private System.Windows.Forms.TextBox txtP2;
+        private System.Windows.Forms.TextBox txtP1;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblP;
+        private System.Windows.Forms.Label lblE;
+        private System.Windows.Forms.Label lblN;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P;
+    }
+}
+
---
+++ Practica 5.1/Practica 5.1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_5._1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+
+            //VariableS
+            int Vp1, Vp2, Vp3;
+            double VPromedio;
+            string VNom;
+            //ingresar datos al dgv
+            int vdgv = dgvDatos.Rows.Add(); ;
+            //entrada de datos
+            VNom = Convert.ToString(txtN.Text);
+            Vp1 = Convert.ToInt32(txtP1.Text);
+            Vp2 = Convert.ToInt32(txtP2.Text);
+            Vp3 = Convert.ToInt32(txtP3.Text);
+            //AGREAR AL DGV
+            dgvDatos.Rows[vdgv].Cells[0].Value = VNom;
+            dgvDatos.Rows[vdgv].Cells[1].Value = Vp1;
+            dgvDatos.Rows[vdgv].Cells[2].Value = Vp2;
+            dgvDatos.Rows[vdgv].Cells[3].Value = Vp3;
+            //CALCULAR PROMEDIO SE SUMAN LAS NOTAS Y SE DIVIDE
+
+            VPromedio = (Vp1 + Vp2 + Vp3) / 3;
+            lblP.Text = VPromedio.ToString();
+
+            //INGRESAMOS UN IF PARA PONER LAS CONDICIONES
+            if (VPromedio >= 95)
+            {
+                MessageBox.Show(" Tu Calificacion es " + VPromedio + " EXENTO :3 ");
+            }
+
+            else if (VPromedio >= 70 && VPromedio < 94)
+            {
+                MessageBox.Show(" Tu Calificacion es " + VPromedio + " ORDINARIO :) ");
+            }
+            else if (VPromedio >= 50 && VPromedio < 69)
+            {
+                MessageBox.Show(" Tu Calificacion es " + VPromedio + " EXTRAORDINARIO :( ");
+            }
+            else if (VPromedio <= 49)
+            {
+                MessageBox.Show(" Tu Calificacion es  " + VPromedio + "  ESPECIAL :C  ");
+            }
+            dgvDatos.Rows[vdgv].Cells[4].Value = VPromedio;
+            
+           
+        } 
+
+        
+          
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            //indicamos que el boton borre los resultados
+     
+            txtN.Text = "";
+
+            txtP1.Text = "";
+            txtP2.Text = "";
+            txtP3.Text = "";
+        }
+
+        private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
